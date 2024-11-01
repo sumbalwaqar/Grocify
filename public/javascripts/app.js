87d860d9291cc30a2628a2f2ce9f456728990a6a
sumbalwaqar <sumbalwaqar80@gmail.com>
2024-11-02 01:41:26 +0500

    laravel-mix installed

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,6 +2,8 @@ const addtoCart = document.querySelectorAll('.add-to-cart');
 
 addtoCart.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-    console.log(e);
+    // console.log(e);
+    const food = JSON.parse(btn.dataset.food);
+    console.log(food);
     });
 });
